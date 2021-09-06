b6415705877b6257533eaa28c657a2e527dfa730
mooch3 <62298384+mooch3@users.noreply.github.com>
2021-09-06 12:27:06 -0700

    added error handling in CLI

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -10,7 +10,15 @@ export const serveCommand = new Command()
     try {
       const dir = path.join(process.cwd(), path.dirname(filename));
       await serve(+options.port, path.basename(filename), dir);
-    } catch (err) {
-        console.log("Here's the problem:", err.message)
+      console.log(
+        `Opened ${filename}. Go to http://localhost:${options.port} to edit the file.`
+      )
+    } catch (err: any) {
+      if (err.code === "EADDRINUS") {
+        console.error('Port is in use, Try running on a different port');
+      } else {
+        console.log("Here's the problem:", err.message);
+      }
+      process.exit(1);
     }
   });
